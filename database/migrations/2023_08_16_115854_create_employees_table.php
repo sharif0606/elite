35e347446e8693e1947fafdea1380612db98fd9f
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-09-30 15:37:59 +0600

    up

--- database/migrations/2023_08_16_115854_create_employees_table.php
+++ database/migrations/2023_08_16_115854_create_employees_table.php
@@ -65,11 +65,14 @@ return new class extends Migration
             $table->string('bn_daughters_name')->nullable();
 
             $table->string('bn_post_allowance')->nullable();
-            $table->string('bn_traning_cost')->comment('detaction by 6 month')->nullable();
+            $table->string('bn_traning_cost')->nullable();
+            $table->string('bn_traning_cost_byMonth')->comment('detaction by 6 month')->nullable();
             $table->string('bn_bank_name')->nullable();
             $table->string('bn_brance_name')->nullable();
             $table->string('bn_ac_no')->nullable();
             $table->string('bn_routing_number')->nullable();
+            $table->decimal('insurance',14,2)->default(130);
+            $table->decimal('p_f',14,2)->comment('by 6 month from joining')->default(200);
 /*End Of Bangla Form */
 
             $table->string('en_applicants_name')->nullable();
