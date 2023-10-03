b2a9f28e0cb8a257af94ccb3c161c9153974775b
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-10-03 20:04:31 +0600

    update

---
+++ app/Models/Salary/SalarySheet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Salary;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SalarySheet extends Model
+{
+    use HasFactory;
+}
