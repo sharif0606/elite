9f1cf51f5ef6af2abde1236fe103506afe30325a
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-10-18 18:40:18 +0600

    update

--- resources/views/invoice_generate/single_show6.blade.php
+++ resources/views/invoice_generate/single_show6.blade.php
@@ -11,8 +11,8 @@
 <body>
     <table width="100%" style="margin-top: 2.2in; font-size: 20px;">
         <tr>
-            <th width="50%">July-2023</th>
-            <th style="text-align: right; padding-right: 50px;">07/08/2023</th>
+            <th width="50%">{{ \Carbon\Carbon::parse($invoice_id->bill_date)->format('F Y')}}</th>
+            <th style="text-align: right; padding-right: 50px;">{{ \Carbon\Carbon::parse($invoice_id->bill_date)->format('d/m/Y') }}</th>
         </tr>
     </table>
     <br>
@@ -21,15 +21,15 @@
     <table width="100%">
         <tr>
             <th width="5%" style="text-align: left;">To:</th>
-            <th style="text-align: left;">COATS Bangladesh Limited</th>
+            <th style="text-align: left;">{{ $invoice_id->customer?->name }}</th>
         </tr>
         <tr>
             <td></td>
-            <td>Fouzderhat I/E, Sagorika Road, Customs Academy,</td>
+            <td>{{ $invoice_id->customer?->brance_name }}</td>
         </tr>
         <tr>
             <td></td>
-            <td>Chittagong,Bangladesh.</td>
+            <td>{{ $invoice_id->customer?->billing_address }}</td>
         </tr>
         <tr>
             <td></td>
@@ -48,52 +48,50 @@
         </tr>
     </table>
     <br>
-    <div><u><b>Subject: Bill for the month of July 2023</b></u></div>
+    <div><u><b>Subject: Bill for the month of {{ \Carbon\Carbon::parse($invoice_id->bill_date)->format('F Y')}}</b></u></div>
     <br>
     <div>Dear Sir</div>
-    <div style="padding-left: 50px;">Bill for the period covering <b>1<sup>st</sup> July 2023 to 31<sup>st</sup>July
-            2023</b> is submitted herewith
+    <div style="padding-left: 50px;">Bill for the period covering <b>{{ \Carbon\Carbon::parse($invoice_id->start_date)->format('d F Y') }}</b> to <b>{{ \Carbon\Carbon::parse($invoice_id->end_date)->format('d F Y') }}</b> is submitted herewith
         please:-</div>
 
     <table width="100%" border="1" cellspacing="0" style="margin-top: 15px;">
-        <tr>
-            <th width="5%">S/N</th>
-            <th width="20%">Service</th>
-            <th width="15%">New Rate <br>per 08 hrs</th>
-            <th width="15%">Period</th>
-            <th width="15%">Total Person</th>
-            <th width="20%">Total Amount br (TK)</th>
-            <th width="10%">Remarks</th>
-        </tr>
-        <tr style="text-align: center;">
-            <td>01.</td>
-            <td>Security Supervisor</td>
-            <td>17,500.00</td>
-            <td><b>July-2023</b> </td>
-            <td>07<br>(217 duties)</td>
-            <td style="text-align: center;">1,22,500.00</td>
-            <td></td>
-        </tr>
-        <tr style="text-align: center;">
-            <td>02.</td>
-            <td>Security Guard</td>
-            <td>15,500.00</td>
-            <td><b>July-2023</b> </td>
-            <td>33<br>(995 duties)</td>
-            <td style="text-align: center;">4,97,500.00</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td colspan="4" style="text-align: right;">Grand Total= </td>
-            <td style="text-align: center;">6,20,000.00</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td colspan="7">Total Amount(In Words): <b><i>Taka Six Lac Twenty Thousand
-                        only.</i></b> </td>
-        </tr>
-
+        <thead>
+            <tr>
+                <th width="5%">S/N</th>
+                <th width="20%">Service</th>
+                <th width="15%">New Rate <br>per 08 hrs</th>
+                <th width="15%">Period</th>
+                <th width="15%">Total Person</th>
+                <th width="20%">Total Amount br (TK)</th>
+                <th width="10%">Remarks</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($invoice_id->details)
+            @foreach ($invoice_id->details as $de)
+            <tr style="text-align: center;">
+                <td>{{ ++$loop->index  }}.</td>
+                <td>{{ $de->jobpost?->name }}</td>
+                <td>{{ $de->rate }}</td>
+                <td><b>{{ \Carbon\Carbon::parse($invoice_id->st_date)->format('F Y')}}</b> </td>
+                <td>{{ $de->employee_qty }}<br>({{ $de->employee_qty*$de->warking_day }} duties)</td>
+                <td style="text-align: center;">1,22,500.00</td>
+                <td></td>
+            </tr>
+            @endforeach
+            @endif
+        </tbody>
+        <tfoot>
+            <tr>
+                <td></td>
+                <td colspan="4" style="text-align: right;">Grand Total= </td>
+                <td style="text-align: center;">6,20,000.00</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="7">Total Amount(In Words): <b><i>Taka Six Lac Twenty Thousand only.</i></b> </td>
+            </tr>
+        </tfoot>
     </table>
     <br>
     <div>The Payment may please be made in Cheques/drafts/cash in favours of <b>'Elite Security Services Limited'</b> by
