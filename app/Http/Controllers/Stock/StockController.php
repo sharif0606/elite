77ad9b04e3e8e2d4ee6be31d3c133eb0d7d3e862
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-11-30 16:07:50 +0600

    up

--- app/Http/Controllers/Stock/StockController.php
+++ app/Http/Controllers/Stock/StockController.php
@@ -17,16 +17,16 @@ class StockController extends Controller
      */
     public function index(Request $request)
     {
-        $product = Product::all();
-        $where = '';
-        if ($request->fdate) {
-            $tdate = $request->tdate ? $request->tdate : $request->fdate;
-            $where = " AND date(stocks.`created_at`) BETWEEN '" . $request->fdate . "' AND '" . $tdate . "'";
-        }
+        // $product = Product::all();
+        // $where = '';
+        // if ($request->fdate) {
+        //     $tdate = $request->tdate ? $request->tdate : $request->fdate;
+        //     $where = " AND date(stocks.`created_at`) BETWEEN '" . $request->fdate . "' AND '" . $tdate . "'";
+        // }
 
-        if ($request->product) {
-            $where .= " AND products.id = '" . $request->product . "'";
-        }
+        // if ($request->product) {
+        //     $where .= " AND products.id = '" . $request->product . "'";
+        // }
 
         //$stock=DB::select("SELECT products.product_name,stocks.product_id,sum(stocks.product_qty) as qty,product_sizes.name FROM `stocks` join products on products.id=stocks.product_id join product_sizes on product_sizes.id=stocks.size_id group by stocks.product_id,stocks.size_id order by stocks.product_id,product_sizes.name");
         $stock=DB::select("SELECT products.product_name,stocks.product_id,sum(stocks.product_qty) as qty,product_sizes.name
