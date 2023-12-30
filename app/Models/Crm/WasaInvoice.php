8848458676a41aa458edfb9059cec648247dbd1d
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-12-30 20:51:49 +0600

    wasa invoice

---
+++ app/Models/Crm/WasaInvoice.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Crm;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WasaInvoice extends Model
+{
+    use HasFactory;
+}
