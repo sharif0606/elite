28733ea9c291c83e4ee839efe19f851557282e03
biplabuddin99 <biplabuddin990@gmail.com>
2024-02-28 21:09:24 +0600

    up

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -1029,11 +1029,16 @@
                 <table style="width: 100%;">
                     <tbody>
                         <tr>
-                            <td style="text-align: left; width: 15%;">আমি, নামঃ</td>
+                            <td class="py-1" style="text-align: left; width: 15%;">আমি, নামঃ</td>
+                            <td class="py-1" style="width: 20%;"><input type="text" class="tinput"  value="{{ $employees->bn_applicants_name }}"></td>
+                            <td class="py-1" style="text-align: center; width: 10%;">পিতাঃ</td>
+                            <td class="py-1" style="width: 15%;"><input type="text" class="tinput"  value="{{ $employees->bn_fathers_name }}"></td>
+                            <td class="py-1" style="text-align: center; width: 40%;">উপরের  উল্লেখিত ১ থেকে ৩০ পর্যন্ত</td>
+                            {{--  <td style="text-align: left; width: 15%;">আমি, নামঃ</td>
                             <td style="width: 20%;">{{ $employees->bn_applicants_name }}</td>
                             <td style="width: 10%;">পিতাঃ</td>
                             <td style="width: 15%;">{{ $employees->bn_fathers_name }}</td>
-                            <td style="width: 40%;">উপরের  উল্লেখিত ১ থেকে ৩০ পর্যন্ত</td>
+                            <td style="width: 40%;">উপরের  উল্লেখিত ১ থেকে ৩০ পর্যন্ত</td>  --}}
                         </tr>
                         <tr>
                             <td colspan="5" style='text-align:justify;'>
