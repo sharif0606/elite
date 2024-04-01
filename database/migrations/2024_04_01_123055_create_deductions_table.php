5fe51c54325e5080519b5b4855290cfc9f4fd3e2
biplabuddin99 <biplabuddin990@gmail.com>
2024-04-01 17:45:35 +0600

    update

--- database/migrations/2024_04_01_123055_create_deductions_table.php
+++ database/migrations/2024_04_01_123055_create_deductions_table.php
@@ -18,17 +18,12 @@ return new class extends Migration
             $table->decimal('fine',10,2)->default(0);
             $table->decimal('mobilebill',10,2)->default(0);
             $table->decimal('loan',10,2)->default(0);
-            $table->decimal('long_loan',10,2)->default(0);
             $table->decimal('cloth',10,2)->default(0);
             $table->decimal('jacket',10,2)->default(0);
             $table->decimal('hr',10,2)->default(0);
-            $table->decimal('traningcost',10,2)->default(0);
             $table->decimal('c_f',10,2)->default(0);
             $table->decimal('medical',10,2)->default(0);
-            $table->decimal('ins',10,2)->default(0);
-            $table->decimal('p_f',10,2)->default(0);
             $table->string('revenue_stamp')->nullable();
-            $table->decimal('total',10,2)->default(0);
             // uncommontow
             $table->decimal('matterss_pillowCost',10,2)->default(0);
             $table->decimal('tonic_sim',10,2)->default(0);
