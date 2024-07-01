f72241fccd086391ec056324a52c27f07e86e8da
Nasim Ahmed <nashim.idb333@gmail.com>
2024-07-01 18:15:06 +0600

    updated invoice design

--- resources/views/invoice_generate/single_show7.blade.php
+++ resources/views/invoice_generate/single_show7.blade.php
@@ -100,7 +100,7 @@
         </tr>
         <tr>
             <td width="15%"></td>
-            <td colspan="2">{!! nl2br(e(str_replace('.', "\n", $branch->billing_address))) !!}</td>
+            <td colspan="2">{!! nl2br(e(str_replace('.', "\n", $branch?->billing_address))) !!}</td>
         </tr>
         @if($branch?->attention)
         <tr>
