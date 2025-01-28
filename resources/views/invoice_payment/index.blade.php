f990affa5db2c5489e5b0e14a064095031e8400e
tawhid12 <tawhid99@gmail.com>
2025-01-28 12:05:27 +0600

    correction

--- resources/views/invoice_payment/index.blade.php
+++ resources/views/invoice_payment/index.blade.php
@@ -78,7 +78,7 @@
                     <tr class="text-center">
                         <th scope="col">{{__('#SL')}}</th>
                         <th scope="col" style="width: 180px;">{{__('Customer Name')}}</th>
-                        <th scope="col" style="width: 60px;">{{__('Month')}}</th>
+                        <th scope="col" style="width: 80px;">{{__('Month')}}</th>
                         <th scope="col">{{__('Billing amount')}}</th>
                         <th scope="col">{{__('Received amount')}}</th>
                         <th scope="col">{{__('Vat %')}}</th>
@@ -100,7 +100,7 @@
                     <tr class="text-center">
                         <td scope="row">{{ ++$loop->index }}</td>
                         <td>{{ $e->customer?->name }}({{$e->invoice?->branch?->brance_name}}) <input type="hidden" value="{{ $e->invoice_id }}"></td>
-                        <td>{{ $e->end_date}}</td>
+                        <td>{{ \Carbon\Carbon::parse($e->invoice?->end_date)->format('M-y') }}</td>
                         <td>{{ $e->received_amount + $e->vat_amount + $e->ait_amount + $e->fine_deduction + $e->paid_by_client + $e->less_paid_honor}}</td>
                         <td>{{ $e->received_amount }}</td>
                         <td>
