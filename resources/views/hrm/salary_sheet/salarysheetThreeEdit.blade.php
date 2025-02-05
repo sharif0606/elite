aed8d50b09f222db3bb0cdf9583c95db3a5f0064
tawhid12 <tawhid99@gmail.com>
2025-02-05 11:40:10 +0600

    correction

--- resources/views/hrm/salary_sheet/salarysheetThreeEdit.blade.php
+++ resources/views/hrm/salary_sheet/salarysheetThreeEdit.blade.php
@@ -349,7 +349,7 @@
 
             if (selectedCustomers.length > 0) {
                 $.ajax({
-                    url: "{{route('get_ajax_salary_branch')}}",
+                    url: "{{route('salarysheet.get_ajax_salary_branch')}}",
                     type: "GET",
                     dataType: "json",
                     data: { customer_ids:selectedCustomers },
