fa692dbadf52a2b7f028a4d8489bd0d1e150f2fd
tawhid12 <tawhid99@gmail.com>
2025-03-02 13:50:56 +0600

    correction

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -294,7 +294,6 @@ class ReportController extends Controller
             } else if (in_array($request->type, [15, 16])) {
                 return view('report.salary-office-staff-prime', compact('getYear', 'getMonth', 'data', 'salaryType'));
             } else if (in_array($request->type, [1, 3, 4, 17, 18])) {
-                echo 3;die;
                 return view('report.salary-details', compact('getYear', 'getMonth', 'data', 'salaryType'));
             } else if (in_array($request->type, [2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])) {
                 return view('report.salary-details-dbbl', compact('getYear', 'getMonth', 'data', 'salaryType'));
