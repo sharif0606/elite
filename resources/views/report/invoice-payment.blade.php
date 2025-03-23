c65da5cc512660f83c9ffbda9adc10c0410752af
tawhid12 <tawhid99@gmail.com>
2025-03-23 15:28:09 +0600

    correction

--- resources/views/report/invoice-payment.blade.php
+++ resources/views/report/invoice-payment.blade.php
@@ -377,15 +377,13 @@
     
     function get_print() {
         $('.full_page').html('<div style="background:rgba(0,0,0,0.5);width:100vw; height:100vh;position:fixed; top:0; left;0"><div class="loader my-5"></div></div>');
-        var year = {{ $getYear }};
-        var month = {{ $getMonth }};
-        var type = {{ $salaryType }};
+       
 
-        $.get("{{route('report.salary_report_details')}}?year=" + year + "&month=" + month + "&type=" + type, function (data) {
+        /*$.get("{{route('report.salary_report_details')}}?year=" + year + "&month=" + month + "&type=" + type, function (data) {
             $("#my-content-div").html(data);
         }).then(function () {
             exportReportToExcel('salaryTable', '{{$name[$salaryType]}}-{{$getMonthName}}-{{$getYear}}');
-        });
+        });*/
     }
 </script>
 @endpush
