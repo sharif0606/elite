69180fa8c48c0910d1030c1bb0e600c2045c54f9
tawhid12 <tawhid99@gmail.com>
2026-01-14 14:22:29 +0600

    correction

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -1406,13 +1406,13 @@
                         <th class="tbl_border" style="text-align: center; padding: 6px;">:</th>
                         <th class="tbl_border" style="padding: 6px;">@if($employees->en_is_criminal_court=='1') {{ 'Yes' }} @elseif($employees->en_is_criminal_court=='2') {{ 'No' }}@else @endif</th>
                     </tr>
-                    <tr class="tbl_border">
+                    {{--<tr class="tbl_border">
                         <th class="tbl_border" style="text-align: center; padding: 6px;">24</th>
                         <th class="tbl_border" style="padding: 6px;">Date of Joining Elite Force</th>
                         <th class="tbl_border" style="text-align: center; padding: 6px;">:</th>
-                        {{-- <th class="tbl_border" style="padding: 6px;">{{$employees->joining_date}}</th> --}}
-                        {{-- <th class="tbl_border" style="padding: 6px;">@if($employees->en_any_other_info=='1') {{ 'Yes' }} @elseif($employees->en_any_other_info=='2') {{ 'No' }}@else @endif</th> --}}
-                    </tr>
+                         <th class="tbl_border" style="padding: 6px;">{{$employees->joining_date}}</th>
+                        <th class="tbl_border" style="padding: 6px;">@if($employees->en_any_other_info=='1') {{ 'Yes' }} @elseif($employees->en_any_other_info=='2') {{ 'No' }}@else @endif</th>
+                    </tr>--}}
                 </tbody>
             </table>
             <div class="row">
