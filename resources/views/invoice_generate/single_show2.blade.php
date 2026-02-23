1ac326f18566f2fcdbdecd7e0cc58dafe94d279d
tawhid12 <tawhid99@gmail.com>
2026-02-23 14:18:17 +0600

    correction

--- resources/views/invoice_generate/single_show2.blade.php
+++ resources/views/invoice_generate/single_show2.blade.php
@@ -303,8 +303,6 @@
                         <td></td>
                         @if($invoice_id->customer_id == 74)
                         <th colspan="{{$invoice_id->detail?->bonus_amount > 0 ?5:8}}">Sub Total</th>
-                        @elseif($invoice_id->customer_id == 13)
-                        <th colspan="4">Sub Total</th>
                         @else
                         <th colspan="{{$invoice_id->detail?->bonus_amount > 0 ?5:4}}">Sub Total</th>
                         @endif
