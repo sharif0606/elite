ea194d51a17f8758add44f3f3d145546694fe800
tawhid12 <tawhid99@gmail.com>
2026-06-17 14:00:36 +0600

    Refactor employee report form layout: Improved styling and structure of the employee report filters, enhancing usability with responsive design and better alignment of input fields.

--- resources/views/Stock/employeeReport/employee_list.blade.php
+++ resources/views/Stock/employeeReport/employee_list.blade.php
@@ -3,57 +3,68 @@
 @section('pageSubTitle',trans('List'))
 
 @section('content')
+<style>
+    .employee-stock-filters .select2-container {
+        width: 100% !important;
+    }
+    .employee-stock-filters input[type="date"] {
+        min-width: 145px;
+    }
+</style>
 <section class="section">
     <form class="form" method="get" action="">
-        <div class="row">
-            <div class="col-12">
-                    <div class="row">
-                        <div class="input-group input-group-sm d-flex justify-content-between flex-wrap gap-2">
-                            <div class="d-flex flex-wrap gap-2 align-items-end">
-                                <select class="form-select employee_id select2" id="employee_id" name="employee_id" style="min-width: 200px;">
-                                    <option value="">Select Employee</option>
-                                    @forelse ($employee as $em)
-                                    <option value="{{ $em->id }}" {{ (request('employee_id') == $em->id ? 'selected' : '') }}>{{ $em->bn_applicants_name .' ('.' Id-'.$em->admission_id_no.')' }}</option>
-                                    @empty
-                                    @endforelse
-                                </select>
-                                <select class="form-select company_id select2" id="company_id" name="company_id" style="min-width: 180px;">
-                                    <option value="">Select Customer</option>
-                                    @forelse ($customer as $c)
-                                    <option value="{{ $c->id }}" @if(request()->get('company_id') == $c->id) selected @endif>{{ $c->name }}</option>
-                                    @empty
-                                    @endforelse
-                                </select>
-                                @if($errors->has('company_id'))
-                                <span class="text-danger"> {{ $errors->first('company_id') }}</span>
-                                @endif
-                                <select class="form-select company_branch_id select2" id="company_branch_id" name="company_branch_id" style="min-width: 160px;">
-                                    <option value="">Select Branch</option>
-                                    @forelse ($branch as $b)
-                                    <option value="{{ $b->id }}" @if(request()->get('company_branch_id') == $b->id) selected @endif>{{ $b->brance_name }}</option>
-                                    @empty
-                                    @endforelse
-                                </select>
-                                @if($errors->has('company_branch_id'))
-                                <span class="text-danger"> {{ $errors->first('company_branch_id') }}</span>
-                                @endif
-                                <div>
-                                    <label for="fdate" class="small mb-0">{{__('From Date')}}</label>
-                                    <input type="date" id="fdate" class="form-control form-control-sm" value="{{ request('fdate') }}" name="fdate">
-                                </div>
-                                <div>
-                                    <label for="tdate" class="small mb-0">{{__('To Date')}}</label>
-                                    <input type="date" id="tdate" class="form-control form-control-sm" value="{{ request('tdate') }}" name="tdate">
-                                </div>
-                                <div class="d-flex align-items-end pb-1">
-                                    <button type="submit" class="btn btn-info">
-                                        <i class="bi bi-search"></i>
-                                    </button>
-                                    <a class="btn btn-warning ms-2" href="{{route('stock.employeeList', ['role' => currentUser()])}}" title="Clear"><i class="bi bi-arrow-clockwise"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div class="row g-2 align-items-end mb-3 flex-lg-nowrap employee-stock-filters">
+            <div class="col-lg col-md-6">
+                <label for="employee_id" class="small mb-1">{{__('Employee')}}</label>
+                <select class="form-select form-select-sm employee_id select2" id="employee_id" name="employee_id">
+                    <option value="">Select Employee</option>
+                    @forelse ($employee as $em)
+                    <option value="{{ $em->id }}" {{ (request('employee_id') == $em->id ? 'selected' : '') }}>{{ $em->bn_applicants_name .' ('.' Id-'.$em->admission_id_no.')' }}</option>
+                    @empty
+                    @endforelse
+                </select>
+            </div>
+            <div class="col-lg col-md-6">
+                <label for="company_id" class="small mb-1">{{__('Customer')}}</label>
+                <select class="form-select form-select-sm company_id select2" id="company_id" name="company_id">
+                    <option value="">Select Customer</option>
+                    @forelse ($customer as $c)
+                    <option value="{{ $c->id }}" @if(request()->get('company_id') == $c->id) selected @endif>{{ $c->name }}</option>
+                    @empty
+                    @endforelse
+                </select>
+                @if($errors->has('company_id'))
+                <span class="text-danger small">{{ $errors->first('company_id') }}</span>
+                @endif
+            </div>
+            <div class="col-lg col-md-6">
+                <label for="company_branch_id" class="small mb-1">{{__('Branch')}}</label>
+                <select class="form-select form-select-sm company_branch_id select2" id="company_branch_id" name="company_branch_id">
+                    <option value="">Select Branch</option>
+                    @forelse ($branch as $b)
+                    <option value="{{ $b->id }}" @if(request()->get('company_branch_id') == $b->id) selected @endif>{{ $b->brance_name }}</option>
+                    @empty
+                    @endforelse
+                </select>
+                @if($errors->has('company_branch_id'))
+                <span class="text-danger small">{{ $errors->first('company_branch_id') }}</span>
+                @endif
+            </div>
+            <div class="col-lg-auto col-md-4">
+                <label for="fdate" class="small mb-1">{{__('From Date')}}</label>
+                <input type="date" id="fdate" class="form-control form-control-sm" value="{{ request('fdate') }}" name="fdate">
+            </div>
+            <div class="col-lg-auto col-md-4">
+                <label for="tdate" class="small mb-1">{{__('To Date')}}</label>
+                <input type="date" id="tdate" class="form-control form-control-sm" value="{{ request('tdate') }}" name="tdate">
+            </div>
+            <div class="col-lg-auto col-md-4 d-flex gap-2 pb-1">
+                <button type="submit" class="btn btn-sm btn-info" title="Search">
+                    <i class="bi bi-search"></i>
+                </button>
+                <a class="btn btn-sm btn-warning" href="{{route('stock.employeeList', ['role' => currentUser()])}}" title="Clear">
+                    <i class="bi bi-arrow-clockwise"></i>
+                </a>
             </div>
         </div>
         <div class="row" id="table-bordered">
